129576a9ffd15c52993a951e3a14325510004af8
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2022-04-21 23:45:11 -0500

    about me finished

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,6 +1,53 @@
 import React from "react";
 import "./about.css";
+import { FaAward } from "react-icons/fa";
+import { FaUsers } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 export default function About() {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know Me</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img
+              src={require("../../assets/AboutMeCropped.png")}
+              alt="About Image"
+            />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FaUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>3200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>Stuff</small>
+            </article>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro amet
+            eius hic quibusdam veniam velit incidunt? Repudiandae rem velit
+            tenetur!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk!
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
